6d5d75f30f423507362965360546454139840354
Volodymyr Bodnarchuk <volodiabodnarchyk2000@gmail.com>
2025-02-17 14:27:26 +0200

    Add Test and Function

--- src/main/java/SumCalculator.java
+++ src/main/java/SumCalculator.java
@@ -1,5 +1,30 @@
 public class SumCalculator {
     public int sum(int n){
-        return 1;
+        if(n <= 0){
+            throw new IllegalArgumentException("Input should be > 0");
+        }
+        int result = 0;
+
+        for (int i = 1; i <= n; i++) {
+            result += i;
+            if(result < 0){
+                throw new IllegalArgumentException("Input " + n + " too big");
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+        int result = 0;
+        for (int i = 1; i < 100000; i++) {
+            result = new SumCalculator().sum(i);
+
+            if(result < 0){
+                System.out.println(i);
+                break;
+            }
+        }
+        System.out.println(result);
     }
 }
--- src/test/java/SumCalculatorTests.java
+++ src/test/java/SumCalculatorTests.java
@@ -1,4 +1,44 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class SumCalculatorTests {
 
+    private SumCalculator calculator;
 
+    @BeforeEach
+    public void beforeEach(){
+        calculator = new SumCalculator();
+    }
+
+    @Test
+    public void testThat0HandledCorrectly(){
+        Assertions.assertThrows(IllegalArgumentException.class , () -> calculator.sum(0));
+    }
+
+    @Test
+    public void testThat1HandledCorrectly(){
+        Assertions.assertEquals(1, calculator.sum(1));
+    }
+
+    @Test
+    public void testThat3HandledCorrectly(){
+        Assertions.assertEquals(6, calculator.sum(3));
+    }
+
+    @Test
+    public void testThat20HandledCorrectly(){
+        Assertions.assertEquals(210, calculator.sum(20));
+    }
+
+    @Test
+    public void testThatNegativeHandledCorrectly(){
+        Assertions.assertThrows(IllegalArgumentException.class , () -> calculator.sum(-5));
+    }
+
+    @Test
+    public void testTooBigInputHandledCorrectly(){
+        int maxN = 65535;
+        Assertions.assertThrows(IllegalArgumentException.class,() -> calculator.sum(maxN + 1));
+    }
 }
